b1e934e128797e08859915c383b3832d5c331744
sorta <orta.sophie@gmail.com>
2016-11-13 18:40:52 -0800

    Added basic icons
    May want to replace these eventually

--- ssoc.js
+++ ssoc.js
@@ -1,4 +1,5 @@
 var express = require('express'),
     logger = require('morgan'),
     app = express(),
+    app.use('/static', express.static(path.join(__dirname, 'public'))),
     app.set('view engine', 'pug');
