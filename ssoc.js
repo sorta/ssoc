be1b4ff84f9dfc05c1fbf3ad6aacdc520747c2f0
sorta <orta.sophie@gmail.com>
2016-11-13 21:22:34 -0800

    Updated build scripts

--- ssoc.js
+++ ssoc.js
@@ -1,5 +1,13 @@
 var express = require('express'),
-    logger = require('morgan'),
-    app = express(),
-    app.use('/static', express.static(path.join(__dirname, 'public'))),
-    app.set('view engine', 'pug');
+  logger = require('morgan'),
+  app = express(),
+  app.use('/static', express.static(path.join(__dirname, 'public'))),
+  app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey' })
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
