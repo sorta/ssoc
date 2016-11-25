e83d8302fbb66d0d69c3bb10721c89c4eaaa5d91
sorta <orta.sophie@gmail.com>
2016-11-24 17:11:33 -0800

    Added fonts and temporary favicon, logo

--- ssoc.js
+++ ssoc.js
@@ -1,9 +1,11 @@
 var express = require('express'),
   logger = require('morgan'),
   path = require('path'),
+  favicon = require('serve-favicon'),
   app = express()
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, '/favicon.ico')))
 
 app.set('view engine', 'pug')
 
