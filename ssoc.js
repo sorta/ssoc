1bffae26fc1fb24a59fac3e1ae5521b35719cfe0
sorta <orta.sophie@gmail.com>
2017-01-05 19:12:12 -0800

    Added about page.

--- ssoc.js
+++ ssoc.js
@@ -5,7 +5,7 @@ var express = require('express'),
   app = express(),
   compression = require('compression')
 
-app.use(compression());
+app.use(compression())
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/static/docs', express.static(path.join(__dirname, 'docs')))
 app.use(favicon(path.join(__dirname, '/favicon.ico')))
@@ -16,6 +16,10 @@ app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey' })
+})
+
+app.get('/about/', function (req, res) {
+  res.render('about', { title: 'Hey' })
 })
 
 app.listen(process.env.PORT || 3000, function () {
