5b2448388362dc4fb5017ccd653872c8cf95e3b5
sorta <orta.sophie@gmail.com>
2017-01-07 16:43:27 -0800

    Added logic for deferring styles

--- publicsrc/scripts/main.js
+++ publicsrc/scripts/main.js
@@ -1,3 +1,11 @@
-var base_doc = document.querySelectorAll('html');
-base_doc.classList.remove('no-js');
-base_doc.classList.add('js');
+var loadDeferredStyles = function() {
+  var addStylesNode = document.getElementById("deferred-styles");
+  var replacement = document.createElement("div");
+  replacement.innerHTML = addStylesNode.textContent;
+  document.body.appendChild(replacement)
+  addStylesNode.parentElement.removeChild(addStylesNode);
+};
+var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+    webkitRequestAnimationFrame || msRequestAnimationFrame;
+if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+else window.addEventListener('load', loadDeferredStyles);
