429097a51181077654f4b966c41c81c68746f4b2
Sophie Orta <orta.sophie@gmail.com>
2018-05-07 11:13:04 -0400

    gulpfile ESLint compliance updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,95 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+const gulp = require('gulp');
 
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-var nodemon = require('gulp-nodemon');
-var imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const nodemon = require('gulp-nodemon');
+const imagemin = require('gulp-imagemin');
 
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 
-var paths = {
-	'styles': {
-		src: './publicsrc/styles/**/*.scss',
-		dest: './public/styles/'
-	},
-	'scripts': {
-		src: ['./publicsrc/scripts/**/*.js'],
-		dest: './public/scripts/'
-	},
-	'images': {
-		src: ['./publicsrc/images/**/*.jpg', './publicsrc/images/**/*.jpeg', './publicsrc/images/**/*.png', './publicsrc/images/**/*.svg'],
-		dest: './public/images/'
-	},
-	'fonts': {
-		src: ['./publicsrc/fonts/**/*.css',
-			'./publicsrc/fonts/**/*.woff',
-			'./publicsrc/fonts/**/*.woff2',
-			'./publicsrc/fonts/**/*.eot',
-			'./publicsrc/fonts/**/*.svg',
-			'./publicsrc/fonts/**/*.ttf'
-		],
-		dest: './public/fonts/'
-	},
+const paths = {
+    styles: {
+        src: './publicsrc/styles/**/*.scss',
+        dest: './public/styles/',
+    },
+    scripts: {
+        src: ['./publicsrc/scripts/**/*.js'],
+        dest: './public/scripts/',
+    },
+    images: {
+        src: ['./publicsrc/images/**/*.jpg', './publicsrc/images/**/*.jpeg', './publicsrc/images/**/*.png', './publicsrc/images/**/*.svg'],
+        dest: './public/images/',
+    },
+    fonts: {
+        src: ['./publicsrc/fonts/**/*.css',
+            './publicsrc/fonts/**/*.woff',
+            './publicsrc/fonts/**/*.woff2',
+            './publicsrc/fonts/**/*.eot',
+            './publicsrc/fonts/**/*.svg',
+            './publicsrc/fonts/**/*.ttf',
+        ],
+        dest: './public/fonts/',
+    },
 
 };
 
-var processors = [
-	autoprefixer,
-	cssnano
+const processors = [
+    autoprefixer,
+    cssnano,
 ];
 
-gulp.task('build:styles', function(){
-	gulp.src(paths.styles.src)
-		.pipe(sourcemaps.init())
-			.pipe(sass().on('error', sass.logError))
-			.pipe(postcss(processors))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(paths.styles.dest));
+gulp.task('build:styles', () => {
+    gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(processors))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.styles.dest));
 });
 
-gulp.task('build:scripts', function(){
-	gulp.src(paths.scripts.src)
-		.pipe(sourcemaps.init())
-			.pipe(uglify())
-			.pipe(rename({suffix: '.min'}))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(paths.scripts.dest));
+gulp.task('build:scripts', () => {
+    gulp.src(paths.scripts.src)
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.scripts.dest));
 });
 
-gulp.task('build:images', function(){
-	gulp.src(paths.images.src)
-		.pipe(imagemin())
-		.pipe(gulp.dest(paths.images.dest));
+gulp.task('build:images', () => {
+    gulp.src(paths.images.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.images.dest));
 });
 
-gulp.task('build:fonts', function(){
-	gulp.src(paths.fonts.src)
-		.pipe(gulp.dest(paths.fonts.dest));
+gulp.task('build:fonts', () => {
+    gulp.src(paths.fonts.src)
+        .pipe(gulp.dest(paths.fonts.dest));
 });
 
 gulp.task('build', ['build:styles', 'build:scripts', 'build:images', 'build:fonts']);
 
-gulp.task('watch:scripts', function () {
-	gulp.watch(paths.scripts.src, ['build:scripts']);
+gulp.task('watch:scripts', () => {
+    gulp.watch(paths.scripts.src, ['build:scripts']);
 });
 
-gulp.task('watch:styles', function () {
-	gulp.watch(paths.styles.src, ['build:styles']);
+gulp.task('watch:styles', () => {
+    gulp.watch(paths.styles.src, ['build:styles']);
 });
 
-gulp.task('watch:images', function () {
-	gulp.watch(paths.images.src, ['build:images']);
+gulp.task('watch:images', () => {
+    gulp.watch(paths.images.src, ['build:images']);
 });
 
 gulp.task('watch', ['watch:styles', 'watch:scripts', 'watch:images']);
 
-gulp.task('serve', function(){
-	nodemon({
-		script: 'ssoc.js'
-	});
+gulp.task('serve', () => {
+    nodemon({ script: 'ssoc.js' });
 });
 
 gulp.task('dev', ['build', 'watch', 'serve']);
