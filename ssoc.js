e9edb57e8671fb888d3ba003eef1ba6159ec6957
Sophie Orta <orta.sophie@gmail.com>
2018-05-07 10:49:55 -0400

    ESLint updates. ES6 updates in main app file.

--- ssoc.js
+++ ssoc.js
@@ -1,45 +1,46 @@
-var express = require('express'),
-  logger = require('morgan'),
-  path = require('path'),
-  favicon = require('serve-favicon'),
-  app = express(),
-  compression = require('compression'),
-  slash = require('express-slash'),
-  uncapitalize = require('express-uncapitalize')
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const compression = require('compression');
+const slash = require('express-slash');
+const uncapitalize = require('express-uncapitalize');
 
-app.use(compression())
-app.use('/static/', express.static(path.join(__dirname, 'public')))
-app.use('/static/docs/', express.static(path.join(__dirname, 'docs')))
-app.use(favicon(path.join(__dirname, '/favicon.ico')))
+const app = express();
+const appPort = process.env.PORT || 3000;
 
-app.enable('strict routing')
-var router = express.Router({
+app.use(compression());
+app.use('/static/', express.static(path.join(__dirname, 'public')));
+app.use('/static/docs/', express.static(path.join(__dirname, 'docs')));
+app.use(favicon(path.join(__dirname, '/favicon.ico')));
+
+app.enable('strict routing');
+const router = express.Router({
     caseSensitive: app.get('case sensitive routing'),
-    strict       : app.get('strict routing')
-})
-app.use(uncapitalize())
-app.use(router)
+    strict: app.get('strict routing'),
+});
+app.use(uncapitalize());
+app.use(router);
 
-app.locals.basedir = __dirname
-app.set('view engine', 'pug')
+app.locals.basedir = __dirname;
+app.set('view engine', 'pug');
 
 // Routes
-app.get('/', function (req, res) {
-  res.render('index', { currentPage: 'Home' })
-})
+app.get('/', (req, res) => {
+    res.render('index', { currentPage: 'Home' });
+});
 
-app.get('/about/', function (req, res) {
-  res.render('about', { currentPage: 'About' })
-})
+app.get('/about/', (req, res) => {
+    res.render('about', { currentPage: 'About' });
+});
 
 // Slashes redirect
-app.use(slash())
+app.use(slash());
 
 // 404
-app.use(function (req, res, next) {
-  res.status(404).render('404', { currentPage: '404' })
-})
+app.use((req, res) => {
+    res.status(404).render('404', { currentPage: '404' });
+});
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
-})
+app.listen(appPort, () => {
+    console.log(`Listening on http://localhost: ${(appPort)}`);
+});
